263c53dbbc2c4729c1e326076ec044c9d39019d6
Zacheus <segunzacheusi@gmail.com>
2026-03-11 17:44:11 +0100

    update config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,14 +1,7 @@
-import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default withSentryConfig(nextConfig, {
-  silent: true,
-  org: "your-sentry-org",
-  project: "sbtc-onramp-aggr",
-  widenClientFileUpload: true,
-  sourcemaps: { disable: true },
-});
+export default nextConfig;
